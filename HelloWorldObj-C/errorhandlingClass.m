11ad995079d62e02c4f455565528968249d49236
Kyle Wilson <xcode.tips.instagram@gmail.com>
2024-03-01 16:23:10 -0500

    nslog Class

--- HelloWorldObj-C/errorhandlingClass.m
+++ HelloWorldObj-C/errorhandlingClass.m
@@ -6,3 +6,79 @@
 //
 
 #import <Foundation/Foundation.h>
+
+//In Objective-C programming, error handling is provided with NSError class available in Foundation framework.
+
+//An NSError object encapsulates richer and more extensible error information than is possible using only an error code or error string. The core attributes of an NSError object are an error domain (represented by a string), a domain-specific error code and a user info dictionary containing application specific information.
+
+/* NSError */
+
+// Objective-C programs use NSError objects to convey information about runtime errors that users need to be informed about. In most cases, a program displays this error information in a dialog or sheet. But it may also interpret the information and either ask the user to attempt to recover from the error or attempt to correct the error on its own
+
+// NSError Object consists of −
+
+// Domain − The error domain can be one of the predefined NSError domains or an arbitrary string describing a custom domain and domain must not be nil.
+
+// Code − The error code for the error.
+
+// User Info − The userInfo dictionary for the error and userInfo may be nil.
+
+@interface ErrorHandlingClass : NSObject
+
+- (NSString *) getEmployeeNameForID:(int) id withError:(NSError **)errorPtr;
+
+@end
+
+@implementation ErrorHandlingClass
+
+- (NSString *) getEmployeeNameForID:(int)id withError:(NSError *__autoreleasing *)errorPtr {
+    if (id == 1) {
+        return @"Employee Test Name";
+    } else {
+        NSString *domain = @"com.MyCompany.MyApplication.ErrorDomain";
+        NSString *desc = @"Unable to complete the process";
+        NSDictionary *userInfo = [[NSDictionary alloc]
+        initWithObjectsAndKeys:desc,
+                                  @"NSLocalizedDescriptionKey", NULL];
+        *errorPtr = [NSError errorWithDomain:domain code:-101 userInfo:userInfo];
+        return @"";
+    }
+}
+
+@end
+
+int main(void) {
+    @autoreleasepool {
+        ErrorHandlingClass *errorHandlingClass = [[ErrorHandlingClass alloc] init];
+        NSError *error = nil;
+        NSString *name1 = [errorHandlingClass getEmployeeNameForID:1 withError:&error];
+        
+        if (error) {
+            NSLog(@"Error finding Name1: %@", error);
+        } else {
+            NSLog(@"Name1: %@", name1);
+        }
+        
+        error = nil;
+        NSString *name2 = [errorHandlingClass getEmployeeNameForID:2 withError:&error];
+        
+        if (error) {
+            NSLog(@"Error finding Name2: %@", error);
+        } else {
+            NSLog(@"Name2: %@", name2);
+        }
+        
+        return 0;
+    }
+}
+
+//In the above example, we return a name if the id is 1, otherwise we set the user-defined error object.
+
+// When the above code is compiled and executed, it produces the following result −
+
+/*
+ 
+ 2013-09-14 18:01:00.809 demo[27632] Name1: Employee Test Name
+ 2013-09-14 18:01:00.809 demo[27632] Error finding Name2: Unable to complete the process
+ 
+ */
--- HelloWorldObj-C/nslogClass.m
+++ HelloWorldObj-C/nslogClass.m
@@ -6,3 +6,44 @@
 //
 
 #import <Foundation/Foundation.h>
+
+@interface NSLogClass : NSObject
+
+- (void)nslogMethod;
+
+@end
+
+@implementation NSLogClass
+
+- (void)nslogMethod {
+    NSLog(@"Hello World");
+}
+
+@end
+
+//Since the NSLogs we use in our application, it will be printed in logs of device and it is not good to print logs in a live build. Hence, we use a type definition for printing logs and we can use them as shown below.
+
+#if DEBUG == 0
+#define DebugLog(...)
+#elif DEBUG == 1
+#define DebugLog(...) NSLog(__VA_ARGS__)
+#endif
+
+int main(void) {
+    DebugLog(@"Debug log, our custom addition gets \
+    printed during debug only");
+    NSLog(@"NSLog gets printed always");
+    return 0;
+}
+
+/*
+ 
+ Now, when we compile and run the program in debug mode, we will get the following result.
+
+2013-09-11 02:47:07.723 demo[618] Debug log, our custom addition gets printed during debug only
+2013-09-11 02:47:07.723 demo[618] NSLog gets printed always
+Now, when we compile and run the program in release mode, we will get the following result.
+
+2013-09-11 02:47:45.248 demo[3158] NSLog gets printed always
+
+*/
