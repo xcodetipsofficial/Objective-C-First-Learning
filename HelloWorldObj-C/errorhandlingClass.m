a17716fd9cef20d83e50258602797f1b32493ef2
Kyle Wilson <xcode.tips.instagram@gmail.com>
2024-03-01 16:23:01 -0500

    errorhandling Class

---
+++ HelloWorldObj-C/errorhandlingClass.m
@@ -0,0 +1,8 @@
+//
+//  errorhandlingClass.m
+//  HelloWorldObj-C
+//
+//  Created by Kyle Wilson on 2024-03-01.
+//
+
+#import <Foundation/Foundation.h>
---
+++ HelloWorldObj-C/nslogClass.m
@@ -0,0 +1,8 @@
+//
+//  nslogClass.m
+//  HelloWorldObj-C
+//
+//  Created by Kyle Wilson on 2024-03-01.
+//
+
+#import <Foundation/Foundation.h>
